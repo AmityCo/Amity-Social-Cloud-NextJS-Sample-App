1b7b27056d0f2f1a693cfb2c621cd45c1baeae93
markamity <worachote@amity.co>
2022-09-29 18:37:11 +0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+
+import { useRouter } from "next/router";
+import dynamic from 'next/dynamic'
+import { useState } from "react";
+const AuthContent = dynamic(() => import('./auth'), {
+  ssr: false,
+})
+const FeedContent = dynamic(() => import('./feed'), {
+  ssr: false,
+})
+
+
+export default function Home({data}) {
+  console.log('data: ', data);
+  const router = useRouter();
+  const [isConnected, setIsConnected] = useState(false)
+  function onConnected(value){
+    setIsConnected(value)
+  }
+  return (
+    <div>
+     <AuthContent onConnected={onConnected}>
+      <FeedContent isConnected={isConnected}/>
+       </AuthContent>
+    </div>
+  );
+}
